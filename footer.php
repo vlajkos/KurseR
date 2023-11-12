30ac2dbc531162469d3058884d17dc02f8b3a1ff
Vladimir <vlajko1453@gmail.com>
2023-11-12 21:52:27 +0100

    Basic Footer Finished

---
+++ footer.php
@@ -0,0 +1,19 @@
+<footer class="footer">
+    <nav>
+        <ul class="icons">
+            <li><a href="https://www.facebook.com/">
+                    <img src="./Images/facebook-icon.png" alt="">
+                </a>
+            <li><a href="https://www.instagram.com/">
+                    <img src="./Images/instagram-icon.png" alt="">
+                </a>
+            <li><a href="https://tiktok.com/">
+                    <img src="./Images/tiktok-icon.png" alt=""> </a>
+            <li><a href="https://www.youtube.com/">
+                    <img src="./Images/youtube-icon.png" alt=""> </a>
+            <li><a href="https://twitter.com/">
+                    <img src="./Images/x-icon.png" alt="" class="icons-x"> </a>
+        </ul>
+    </nav>
+    <p>&copy; 2023 Anđela</p>
+</footer>
--- index.php
+++ index.php
@@ -20,7 +20,12 @@
 <body>
 <?php
     include 'header.php';
- ?>
-
+?>
+<h1>Ovo je body</h1>
 </body>
+<footer>
+<?php
+    include 'footer.php';
+ ?>
+</footer>
 </html>
